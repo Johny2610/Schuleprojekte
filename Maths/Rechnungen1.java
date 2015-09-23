1b4b3e4749f4b0f0df0fc938d481f0cc8724f407
Johannes Weiß <Johny2610@users.noreply.github.com>
2015-09-23 15:02:03 +0200

    Sorting Operations

--- Maths/Rechnungen1.java
+++ Maths/Rechnungen1.java
@@ -9,15 +9,6 @@
 public class Rechnungen1 {
   
   public static void main(String[] args) {
-    System.out.print("Geben Sie die erste Zahl ein: ");
-    double zahl1 ;
-    zahl1 =Tastatur.leseKommazahl();
-    System.out.println("zahl1 ist: "+zahl1);
-    
-    System.out.print("Geben Sie die zweite Zahl ein: ");
-    double zahl2 ;
-    zahl2 =Tastatur.leseKommazahl();
-    System.out.println("zahl2 = "+zahl2);
     
     System.out.println("Wählen Sie den gewünschten Operator: ");
     System.out.println("1 = Addition");
@@ -30,46 +21,55 @@ public class Rechnungen1 {
     int operator;
     operator = Tastatur.leseZahl();
     
-    double ergebnis1 ;
-    double ergebnis2 ;
-    double ergebnis3 ;
-    double ergebnis4 ;
-    double ergebnis5 ;
-    double ergebnis6 ;
+    System.out.print("Geben Sie die erste Zahl ein: ");
+    double zahl1 ;
+    zahl1 =Tastatur.leseKommazahl();
+    System.out.println("zahl1 ist: "+zahl1);
+    
+    System.out.print("Geben Sie die zweite Zahl ein: ");
+    double zahl2 ;
+    zahl2 =Tastatur.leseKommazahl();
+    System.out.println("zahl2 = "+zahl2);
     
     if (operator == 1) {
-      ergebnis1 = zahl1+zahl2 ;
+      double ergebnis1;
+      ergebnis1 = zahl1+zahl2;
       System.out.println("zahl1 + zahl2 = "+zahl1+" + "+zahl2+" = "+ergebnis1);  
     } else if (operator == 2) {
-      ergebnis2 = zahl1-zahl2 ;
+      double ergebnis2;
+      ergebnis2 = zahl1-zahl2;
       System.out.println("zahl1 - zahl2 = "+zahl1+" - "+zahl2+" = "+ergebnis2);  
     } else if (operator == 3) {
-      ergebnis3 = zahl1*zahl2 ;
+      double ergebnis3;
+      ergebnis3 = zahl1*zahl2;
       System.out.println("zahl1 * zahl2 = "+zahl1+" x "+zahl2+" = "+ergebnis3);    
     } else if (operator == 4) {
       if (zahl2 == 0){
         System.out.println("ERROR Divisor = 0");
       } 
       else{
+        double ergebnis4;
         ergebnis4 = zahl1/zahl2;
         System.out.println("zahl1 / zahl2 = "+zahl1+" : "+zahl2+" = "+ergebnis4);
       }
     } else if (operator == 5) {
       if (zahl2 < 0){
-         System.out.println("ERROR Number < 0");
+        System.out.println("ERROR Number < 0");
       } 
       else{
+        double ergebnis5;
         double operation = 1.0/zahl1; 
         ergebnis5 = Math.pow(zahl2, operation);                  
         System.out.println("zahl1-te Wurzel aus zahl2 = "+zahl1+"-te Wurzel aus "+zahl2+" = "+ergebnis5);
       }                    
     } else if (operator == 6) {
+      double ergebnis6;
       ergebnis6 = Math.pow(zahl1, zahl2);                  
       System.out.println("zahl1 ^ zahl2 = "+zahl1+" ^ "+zahl2+" = "+ergebnis6);
     } else {
-      System.out.println("Fehlerhafter Operator gewählt!"); 
+      System.out.println("Fehlerhaften Operator gewählt!"); 
     } // end of if-else
-  
+    
   }  // end of main
   
 } // end of class Rechnungen1
