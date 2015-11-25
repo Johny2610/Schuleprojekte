2c3b81d97155e091d72f402e7b690098f94f1e61
Johannes Weiß <Johny2610@users.noreply.github.com>
2015-11-25 18:21:58 +0100

    Added new Project

--- Widerstandsberechnung/Widerstandsberechnung.java
+++ Widerstandsberechnung/Widerstandsberechnung.java
@@ -1,33 +1,35 @@
 // Autor:   Johannes Julius Weiss
-// Datum:   19.11.2015
-// Version: 1.0
+// Datum:   20.11.2015
+// Version: 1.1
 
 public class Widerstandsberechnung {
   
   public static void main(String[] args) {
-    System.out.println("// Title:   Widerstandsberechnung //");
+    System.out.println("// Titel:   Widerstandsberechnung //");
     System.out.println("// Autor:   Johannes Weiss        //");
-    System.out.println("// Datum:   19.11.2015            //");
-    System.out.println("// Version: 1.0                   //\n\n");
-    System.out.print("Geben Sie die Anzahl der Messreihen ein:           ");
-    int x = Tastatur.leseZahl();
-    if (x > 0) {
+    System.out.println("// Erstelldatum:   19.11.2015     //");
+    System.out.println("// Änderungsdatum:   20.11.2015   //");
+    System.out.println("// Version: 1.1                   //\n\n");
+    System.out.print("Geben Sie die Anzahl der Messreihen ein:            ");
+    int anzahlMessreihen = Tastatur.leseZahl();
+    if (anzahlMessreihen > 0) {
       int laufvariable = 1;
       double summeWiderstand = 0;
       double mittelwertWiderstand = 0;
-      while (laufvariable <= x) { 
-        System.out.print("Geben Sie die elektrische Spannung U"+laufvariable+" ein:         ");
+      while (laufvariable <= anzahlMessreihen) { 
+        System.out.print("Geben Sie die elektrische Spannung U"+laufvariable+" ein:          ");
         double spannung = Tastatur.leseKommazahl();
-        System.out.print("Geben Sie die elektrische Strom I"+laufvariable+" ein:            ");
+        System.out.print("Geben Sie die elektrische Strom I"+laufvariable+" ein:             ");
         double strom = Tastatur.leseKommazahl();
         double widerstand = spannung / strom;
-        System.out.println("Der Widerstand der Messreihe "+laufvariable+" beträgt:            "+widerstand+"\n");
+        System.out.println("Der Widerstand der Messreihe "+laufvariable+" beträgt:             "+widerstand+"\n");
         summeWiderstand = summeWiderstand + widerstand;
         mittelwertWiderstand = summeWiderstand / laufvariable;
-        System.out.println("Der bisherige Mittelwert der Widerstände beträgt:  "+mittelwertWiderstand+"\n");
+        System.out.println("Der bisherige Mittelwert der Widerstände beträgt:   "+mittelwertWiderstand+"\n");
         laufvariable++;
       }
-      System.out.println("Der endgueltige Mittelwert des Widerstandes beträgt: "+mittelwertWiderstand);
+      mittelwertWiderstand = summeWiderstand / anzahlMessreihen;
+      System.out.println("Der endgültige Mittelwert des Widerstandes beträgt: "+mittelwertWiderstand);
     } else {
       System.err.println("ERROR: Anzahl der Messreihen <= 0");
     } 
---
+++ Widerstandsberechnung/Widerstandsberechnung_for.java
@@ -0,0 +1,30 @@
+// Autor:   Johannes Julius Weiss
+// Datum:   20.11.2015
+// Version: 1.0
+
+public class Widerstandsberechnung_for {
+  
+  public static void main(String[] args) {
+    System.out.println("// Titel:   Widerstandsberechnung  //");
+    System.out.println("// Autor:   Johannes Weiss         //");
+    System.out.println("// Datum:   20.11.2015             //");
+    System.out.println("// Version: 1.0                    //\n\n");
+    System.out.print("Geben Sie die Anzahl der Messreihen ein:            ");
+    int anzahlMessreihen = Tastatur.leseZahl();
+    if (anzahlMessreihen > 0) {
+      double summeWiderstand = 0;
+      double mittelwertWiderstand = 0;
+      for (int laufvariable = 1; laufvariable <= anzahlMessreihen; laufvariable++) { 
+        System.out.print("Geben Sie den elek. Widerstand R"+laufvariable+" ein:              ");
+        double widerstand = Tastatur.leseKommazahl();                                                                                    
+        summeWiderstand = summeWiderstand + widerstand;
+        mittelwertWiderstand = summeWiderstand / laufvariable;
+        System.out.println("Der bisherige Mittelwert der Widerstände beträgt:   "+mittelwertWiderstand+"\n");
+      }
+      mittelwertWiderstand = summeWiderstand / anzahlMessreihen;
+      System.out.println("Der endgültige Mittelwert des Widerstandes beträgt: "+mittelwertWiderstand);
+    } else {
+      System.err.println("ERROR: Anzahl der Messreihen <= 0");
+    } 
+  }  
+}
